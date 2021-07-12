5fe57792657fa0dbe7bac70ed334f4f4412ed331
Elizabeth Davis <elizabeth.davis@improving.com>
2021-07-12 08:21:32 -0500

    Step 1

---
+++ FancyCalculator/Input.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FancyCalculator
+{
+   public class Input
+    {
+        public decimal Input1 { get; set; }
+     public   decimal Input2 { get; set; }
+
+    }
+}
---
+++ FancyCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FancyCalculator;
+namespace FancyCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Input input = new Input();
+            decimal strInput1 = Decimal.Zero;
+            decimal strInput2 = Decimal.Zero;
+
+            Console.WriteLine("Please enter two numeric values.");
+            Console.WriteLine("Please enter the first number.");
+
+            bool success =  Decimal.TryParse(Console.ReadLine(),out strInput1);
+            if (success)
+            {
+                input.Input1 = strInput1;
+                Console.WriteLine("Please enter the second number.");
+
+                 success = Decimal.TryParse(Console.ReadLine(), out strInput2);
+                if (success)
+                {
+                    input.Input2 = strInput2;
+                    Console.WriteLine("The sum is:{0}",input.Input1+input.Input2);
+
+                }
+            }
+
+        }
+    }
+}
